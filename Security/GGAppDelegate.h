075fb1db3d7109280a4ea1d7f43127f076bcbdab
Dola's MAC <admin@dola-2.default>
2013-09-09 17:57:39 +0100

    Initial Commit

---
+++ Security/GGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GGAppDelegate.h
+//  Security
+//
+//  Created by Dola's MAC on 09/09/2013.
+//  Copyright (c) 2013 GivtGram. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
